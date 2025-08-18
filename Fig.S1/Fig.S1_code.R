9f64ed9628f8de3f579b56bccc8955f5cad70fe8
Mengyue-Li <limyhzau@163.com>
2025-08-18 14:10:19 +0800

    Add files via upload

---
+++ Fig.S1/Fig.S1_code.R
@@ -0,0 +1,27 @@
+
+###########################################################################################
+#####                                    Fig.S1                                       #####
+###########################################################################################
+
+library(pheatmap)
+setwd('C:/Users/MY/Desktop/CODE/Fig.S1&2')
+
+data1 = read.xlsx("data_Fig.S1&2.xlsx", sheet = "FigS1", rowNames = T, colNames = T)
+head(data1)
+dim(data1)
+data= data.matrix(data1) 
+
+pheatmap(data1,
+         cluster_cols = F, cluster_rows = F, scale = "none", 
+         treeheight_col = 0, treeheight_row = 0, 
+         display_numbers = F, 
+         legend = FALSE,
+         #angle_col = 0,
+         border_color ="#A2A2A2", 
+         show_colnames = T,
+         show_rownames = F,
+         fontsize_col = 9,  fontsize_row = 9,
+         color = colorRampPalette(c( "#FFFFFF" , "grey"))(2),  
+         cellwidth =5,  cellheight = 5) -> pS1;pS1
+
+
